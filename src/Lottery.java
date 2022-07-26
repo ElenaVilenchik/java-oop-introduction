495a3cb3d7ed5ba6ed260683e2fb65bd76602680
User <User@User-PC.Dlink>
2022-07-26 13:29:19 +0300

    HW - Task05_Lottery --> HashSet

--- src/Lottery.java
+++ src/Lottery.java
@@ -1,54 +1,39 @@
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * This class represents Lottery 7 of 49 The purpose of this task is to
  * implement rateCombination() with complexity O[N], where N=7
  */
 public class Lottery {
-	private int[] winningCombination;
-	/**
-	 * Constructor
-	 * @param winningCombination - array of 7 unique numbers in range [1 to 49] (no need to check)        
-	 */
+	private static int[] winningCombination;
+
 	public Lottery(int[] winningCombination) {
-		int[] lut = new int[50];
-		for (int i = 0; i < winningCombination.length; i++) {
-			lut[winningCombination[i]] = winningCombination[i];
-		}
-		this.winningCombination = lut;
+		Lottery.winningCombination = winningCombination;
 	}
 
-	/**
-	 * Rates player's combination of balls, comparing it with winner combination
-	 * 
-	 * @param playerCombination - array of 7 unique numbers in range [1 to 49] (no
-	 *                          need to check)
-	 * @return number of winning balls in player's combination
-	 */
 	public int rateCombination(int playerCombination[]) {
-		// TODO : return rate in O[N], where N=7. For example:
-		// winningComination 5,2,17,48,43,7,9
-		// playerCmbination: 3,5,12,17,44,10,7 ==> rate 3 (because 5,17,7 are hit)
+		Set<Integer> playerCombinationSet = new HashSet<>();
 
-		int rate = 0;
 		for (int i = 0; i < playerCombination.length; i++) {
-			if ( winningCombination[playerCombination[i]]!=0) {
-				rate++;
-			}
+			playerCombinationSet.add(playerCombination[i]);
+			playerCombinationSet.add(winningCombination[i]);
 		}
-		return rate;
+		return playerCombination.length * 2 - playerCombinationSet.size();
 	}
 
 	/**
 	 * Simple sanity test
 	 */
 	public static void main(String[] args) {
-		Lottery loto = new Lottery(new int[] { 5, 2, 17, 48, 43, 7, 9 });
-		if (loto.rateCombination(new int[] { 3, 5, 12, 17, 44, 10, 7 }) != 3) {
+		Lottery loto = new Lottery(new int[]{5,2,17,48,43,7,9}) ;
+		if (loto.rateCombination(new int[] {3,5,12,17,44,10,7}) != 3) {
 			System.out.println("Test 1 failed");
-			return;
+            return;			
 		}
-		if (loto.rateCombination(new int[] { 3, 5, 12, 17, 49, 9, 7 }) != 5) {
+		if (loto.rateCombination(new int[] {3,5,12,17,49,9,7}) != 5) {
 			System.out.println("Test 2 failed");
-			return;
+            return;			
 		}
 		System.out.println("Success");
 	}
